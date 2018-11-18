e53b03ce02f7fda3341da7ae8eed21134dc1c74e
circular-code <stephanos@malikotsinas>
2018-11-18 15:07:53 +0100

    circle saver

--- app.js
+++ app.js
@@ -6,14 +6,43 @@ function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   background(0);
   stroke('#ffffff');
+  strokeWeight(20);
+  frameRate(30);
   fill(0,0,0,0);
 }
 
-var i = 0, j = 0, k = 10;
+var circles = [];
+
+function Circle(x, y, r) {
+  this.x = x;
+  this.y = y;
+  this.r = r;
+  this.colorR = 255 * random();
+  this.colorG = 255 * random();
+  this.colorB = 255 * random();
+  this.colorA = 255 * random();
+
+  this.increaseSize = function() {
+    if (this.r < windowWidth * 1.5) {
+      this.r += 20;
+      this.y += 5;
+    }
+    else {
+      circles.shift();
+    }
+  }
+}
 
 function draw() {
-    stroke(255 * Math.random(), 255 * Math.random(), 255 * Math.random(), 255 * Math.random());
-    ellipse(mouseX, mouseY, k);
+    clear();
+    background(0);
+    random() < 0.5 ? circles.push(new Circle(windowWidth / 2, -200, 100)) : '';
+
+    circles.forEach(function(circle) {
+      stroke(0, 100, 200, 100);
+      circle.increaseSize.bind(circle)();
+      ellipse(circle.x, circle.y, circle.r);
+    });
 }
 
 window.addEventListener('keydown', function(e) {
@@ -23,6 +52,10 @@ window.addEventListener('keydown', function(e) {
     k -= 5;
 });
 
+function mouseClicked() {
+  redraw();
+}
+
 function randomSign() {
-  return Math.random() < 0.5 ? -1 : 1;
+  return random() < 0.5 ? -1 : 1;
 }
